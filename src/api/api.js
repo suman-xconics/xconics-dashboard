fff8cfeef6d70fb49a7ee943aabc1633c5592eaf
Suman Mondal <mondalsumanrock20@gmail.com>
2026-02-05 12:06:22 +0530

    fix: Update API base URL to the production server

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://172.105.36.66:8020",
     headers: {
         "Content-Type": "application/json",
     },
